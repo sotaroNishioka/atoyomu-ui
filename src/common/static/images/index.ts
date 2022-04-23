97c0844853e62b4e66d72808ddf50f923fbfb988
sotaroNishioka <nishiokanandesuka@gmail.com>
2022-04-23 20:02:04 +0900

    env更新

--- src/common/static/images/index.ts
+++ src/common/static/images/index.ts
@@ -1,6 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 import applogo from './applogo.svg' // Contact.ts
-import sendmail from './sendmail.svg'
 
 export const AppLogo = applogo
-export const SendMail = sendmail
