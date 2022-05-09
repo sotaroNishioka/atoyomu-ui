5ecf0b7cbe699898d180196bb852376c1954c4c7
sotaroNishioka <nishiokanandesuka@gmail.com>
2022-05-10 00:55:21 +0900

    AuthRouteにリダイレクト先選択を追加

--- src/components/ui-parts/AuthRoute.tsx
+++ src/components/ui-parts/AuthRoute.tsx
@@ -2,12 +2,18 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import Router from 'next/router'
 import { ReactElement } from 'react'
 
-const AuthRoute = ({ children }: { children: ReactElement<any, any> }) => {
+const AuthRoute = ({
+  children,
+  route
+}: {
+  children: ReactElement<any, any>
+  route?: string
+}) => {
   const auth = getAuth()
 
   onAuthStateChanged(auth, (user) => {
     if (user === null) {
-      Router.replace('/')
+      Router.replace(route === undefined ? '/' : route)
     }
   })
 
