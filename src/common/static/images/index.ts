6956894372b92765ee4380dc3ec7865043c90de6
sotaroNishioka <nishiokanandesuka@gmail.com>
2022-05-09 10:55:38 +0900

    ビルドエラー修正

--- src/common/static/images/index.ts
+++ src/common/static/images/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import applogo from './applogo.svg'
 import check from './check.svg'
-import sendMail from './sendMail.svg'
+import sendMail from './sendmail.svg'
 
 export const AppLogo = applogo
 export const SendMail = sendMail
